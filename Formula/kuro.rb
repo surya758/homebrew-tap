d394b6b928c0950f7d03d6696ef3441952ba0cd0
skant758 <skant758@gmail.com>
2026-08-13 21:04:00 +0530

    Bump kuro to v0.2.1

--- Formula/kuro.rb
+++ Formula/kuro.rb
@@ -13,8 +13,8 @@ class Kuro < Formula
   desc "Terminal anime streaming client that plays in IINA"
   homepage "https://github.com/surya758/kuro"
   url "https://github.com/surya758/kuro.git",
-      tag:      "v0.2.0",
-      revision: "d77f11ef6d08b8b43f45c7a67a124271249ba296"
+      tag:      "v0.2.1",
+      revision: "a130982e1d96ab01736486ae3fbf2f47b397c498"
   license "MIT"
   head "https://github.com/surya758/kuro.git", branch: "main"
 
