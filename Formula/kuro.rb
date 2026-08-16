36d1d8a60aa3deb460193b49b614816ac7617b86
skant758 <skant758@gmail.com>
2026-08-16 23:02:50 +0530

    kuro 0.6.3

--- Formula/kuro.rb
+++ Formula/kuro.rb
@@ -13,8 +13,8 @@ class Kuro < Formula
   desc "Terminal anime streaming client that plays in IINA"
   homepage "https://github.com/surya758/kuro"
   url "https://github.com/surya758/kuro.git",
-      tag:      "v0.6.2",
-      revision: "9515591d47a858e76628a0e052486e4e65baff75"
+      tag:      "v0.6.3",
+      revision: "9dcb59e91c60b6bfef18e60440666fcf05a14f3c"
   license "MIT"
   head "https://github.com/surya758/kuro.git", branch: "main"
 
